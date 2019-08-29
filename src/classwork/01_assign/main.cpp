f29e8416a5e132a6467cd7b96b8f39776f07ab22
Kien Yee <kien.yee@g.austincc.edu>
2019-08-28 19:15:00 -0500

    Add test case and main code

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -1,12 +1,15 @@
 //Write the include statement for types.h here
-
+#include "types.h"
 //Write include for capturing input from keyboard and displaying output to screen
-
+#include <iostream>
+using std::cout;
 
 
 int main() 
 {
+	int result = sum_numbers(5, 5);
 
+	cout << "Result: " << result;
 	return 0;
 }
 
