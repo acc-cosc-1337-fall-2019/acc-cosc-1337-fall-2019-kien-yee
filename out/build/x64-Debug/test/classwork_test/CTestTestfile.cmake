5d8bec3c6c78a86a6b4c9f68cf88b4d5dac47efc
Kien Yee <Kien Yee@DESKTOP-HHQLM4V>
2019-09-04 00:04:36 -0500

    homework 1 complete

---
+++ out/build/x64-Debug/test/classwork_test/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Kien Yee/source/repos/acc-cosc-1337-fall-2019-kien-yee/test/classwork_test
+# Build directory: C:/Users/Kien Yee/source/repos/acc-cosc-1337-fall-2019-kien-yee/out/build/x64-Debug/test/classwork_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("01_assign_test")
