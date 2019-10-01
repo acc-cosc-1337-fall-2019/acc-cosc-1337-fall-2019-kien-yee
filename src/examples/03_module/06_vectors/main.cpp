a332faeb3ace4755827e95793b949672de09af45
Kien Yee <kien.yee@g.austincc.edu>
2019-09-30 19:22:17 -0500

    9/30 lecture Classes

--- src/examples/03_module/06_vectors/main.cpp
+++ src/examples/03_module/06_vectors/main.cpp
@@ -1,10 +1,13 @@
+#include<string>
 #include "vec.h"
 
 int main() 
 {
 	loop_vector_w_index();
-
+	std::vector<int> result = ret_vec_int();
 	std::vector<int> nums = { 99,55,1,3,66 };
+	std::vector <std::string> strs(9, " ");
+
 	loop_vector_w_index(nums);
 
 	loop_vector_w_index_ref(nums);
