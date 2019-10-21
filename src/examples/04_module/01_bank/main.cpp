d303c51d21aa47dce1c533c85cb32a3c6d0c5ea8
Kien Yee <kien.yee@g.austincc.edu>
2019-10-21 18:22:55 -0500

    vector reference wrapper objects

--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -1,3 +1,4 @@
+#include<functional>
 #include<iostream>
 #include<vector>
 #include"atm.h"
@@ -9,41 +10,20 @@
 using std::cout;
 using std::vector;
 using std::cin;
+using std::reference_wrapper;
 
 int main()
 {
+	CheckingAccount c(1500);
+	SavingsAccount s(500);
 
-	/*BankAccount a;
-	BankAccount b = a;
-	display(a);//friend function
+
+	vector<reference_wrapper<BankAccount>> accounts{c, s};
 	
-	vector<BankAccount> accounts{ BankAccount(500),BankAccount(1000),BankAccount(600) };
-	Cust
-	
-	
-	ATM atm(accounts[0]);
-	atm.deposit(100);
-	atm.withdraw(50);
-	atm.display_balance();
-	cin >> a
-	cout << a;*/
-
-	/*Customer customer;
-	customer.add_account(BankAccount(500));
-	customer.add_account(BankAccount(1500));
-	customer.add_account(BankAccount(5000));
-
-	cout << customer;*/
-	CheckingAccount checking(1500);
-
-	cout << "\nChecking get_balance " << checking.get_balance() << "\n";
-
-	SavingsAccount savings(500);
-	
-	cout << "\nSavings get balance " << savings.get_balance() << "\n";
-
-	BankAccount& account = savings;
-	cout << "\nRef to savings get_balance: " << account.get_balance() << "\n";
+	for (auto account : accounts)
+	{
+		cout << "Balance: " << account.get().get_balance() << "\n";
+	}
 	
 	return 0;
 }
