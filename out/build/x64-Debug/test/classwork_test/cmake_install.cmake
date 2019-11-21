b64489d56577555e3ae4efbf7dd00dc464ff1de8
Kien Yee <Kien Yee@DESKTOP-HHQLM4V>
2019-11-20 19:13:27 -0600

    11/20 Vector assignment 13

--- out/build/x64-Debug/test/classwork_test/cmake_install.cmake
+++ out/build/x64-Debug/test/classwork_test/cmake_install.cmake
@@ -32,3 +32,9 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("C:/Users/Kien Yee/source/repos/acc-cosc-1337-fall-2019-kien-yee/out/build/x64-Debug/test/classwork_test/11_assign_test/cmake_install.cmake")
+
+endif()
+
